8ca70c2f9b600d213709fe683d39e9ab31537c54
nuttycc <73019368+nuttycc@users.noreply.github.com>
2025-04-19 13:33:47 +0800

    chore(config): update compatibility date for Nuxt configuration
    - Change compatibilityDate from '2024-11-01' to '2025-04-01' in nuxt.config.ts to reflect updated support timeline

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-11-01',
+  compatibilityDate: '2025-04-01',
   devtools: {
     enabled: true,
 
